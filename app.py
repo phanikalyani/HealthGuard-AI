23530d1851b8322eb76cf53a60ac08808b3e382b
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-04-05 21:01:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-
-from db import *
-
-# ---------------- PAGE CONFIG ----------------
-st.set_page_config(page_title="HealthGuard AI", layout="centered")
-
-st.title("🏥 HealthGuard AI")
-st.subheader("Predicting Health Risks Before It’s Too Late")
-
-st.markdown("""
-*This tool provides an AI-based health risk estimate 
-and is NOT a medical diagnosisimport streamlit as st""")
 import pandas as pd
 import numpy as np
 import os
