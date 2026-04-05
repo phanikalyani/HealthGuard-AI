9b9998e2073ea3d3d6fade8cca8364acec11b516
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-04-05 19:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,8 @@ st.set_page_config(page_title="HealthGuard AI", layout="centered")
 st.title("🏥 HealthGuard AI")
 st.subheader("Predicting Health Risks Before It’s Too Late")
 
-st.markdown("""
-*This tool provides an AI-based health risk estimate 
-and is NOT a medical diagnosisPlease consult a doctor for professional medical advice.*
-""")
+st.markdown(""" *This tool provides an AI-based health risk estimate 
+            and is NOT a medical diagnosisPlease consult a doctor for professional medical advice.* """)
 
 # ---------------- SESSION ----------------
 if "user" not in st.session_state:
